267c156d3f8f2865daba65a4cd84a1fe9e1e95f8
Alexandros Bantis <alexandros_bantis@apple.com>
2016-09-13 11:23:13 -0700

    1.2.5

--- src/sicp_clojure/c1_2.clj
+++ src/sicp_clojure/c1_2.clj
@@ -557,11 +557,51 @@
 ;; Exercise 1.20: The process that a procedure generates is of course dependent
 ;; on the rules used by the interpreter.  As an example, consider the iterative
 ;; `gcd' procedure given above.  Suppose we were to interpret this procedure
-;; using normal-order evaluation, as discussed in section *Note 1-1-5::.  (The
-;; normal-order-evaluation rule for `if' is described in *Note Exercise 1-5::.)
+;; using normal-order evaluation, as discussed in section 1-1-5.  (The
+;; normal-order-evaluation rule for `if' is described in *Exercise 1-5.)
 ;; Using the substitution method (for normal order), illustrate the process
 ;; generated in evaluating `(gcd 206 40)' and indicate the `remainder'
 ;; operations that are actually performed.  How many `remainder' operations are
 ;; actually performed in the normal-order evaluation of `(gcd 206 40)'?  In the
 ;; applicative-order evaluation?
 
+;; "normal-order-evaluation" is lazy:
+;; (gcd 206 40)
+
+;; (gcd 40 (rem 206 40))
+
+;; (gcd (rem 206 40) (rem 40 (rem 206 40)))
+
+;; (gcd (rem 40
+;;           (rem 206
+;;                40))
+;;      (rem (rem 206
+;;                40)
+;;           (rem 40
+;;                (rem 206
+;;                     40))))
+
+;; (gcd (rem (rem 206
+;;                40)
+;;           (rem 40
+;;                (rem 206
+;;                     40)))
+;;      (rem (rem 40
+;;                (rem 206
+;;                     40))
+;;           (rem (rem 206
+;;                     40)
+;;                (rem 40
+;;                     (rem 206
+;;                          40)))))
+
+;; "applicative-order-evaluation" (aka eager):
+;; (gcd 206 40)
+;; (gcd 40 6)
+;; (gcd 6 4)
+;; (gcd 4 2)
+;; (gcd 2 0)
+;; 2
+
+;; Thus, for applicative-order-evaluation, rem is called 5 times, for
+;; normal-order-evaluation, it is called 11 times.
